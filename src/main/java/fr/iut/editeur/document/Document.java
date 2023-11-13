77538ef23304de45db5271a9d814aafd9c326ad9
Diego.C <diego.carriere@etu.umontpellier.fr>
2023-11-13 11:37:09 +0100

    inserer finit

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -37,7 +37,6 @@ public class Document {
         String toUpper = texte.substring(start, end).toUpperCase();
         texte = leftPart + toUpper + rightPart;
     }
-
     public void effacer(int start, int end) {
         String leftPart = texte.substring(0, start);
         String rightPart = texte.substring(end);
